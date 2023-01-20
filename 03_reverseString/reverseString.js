1af30524796923269449c42ed6ab0102464ca648
gabr1el1 <gabrielhgxd@gmail.com>
2023-01-20 17:35:43 -0600

    Exercise done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let splitString = string.split("");
+    let reverseString = "";
+    for(let i = splitString.length-1; i >= 0; i--){
+        reverseString+=splitString[i];
+    }
+    return reverseString;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
 const removeFromArray = function() {
-
+    let newArray = arguments[0];
+    let removePosition;
+    //console.log(search);
+    for(let i = 1;i < arguments.length;i++){
+        removePosition = newArray.indexOf(arguments[i]);
+        if(removePosition!=-1){
+            newArray.splice(removePosition,1);
+        }
+    }
+    return newArray;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
