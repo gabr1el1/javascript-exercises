a07df041927506b02dbd068546e5491833828465
gabr1el1 <gabrielhgxd@gmail.com>
2023-01-22 17:11:02 -0600

    Exercise done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(n1, n2) {
+    if(typeof n1!=='number' || typeof n2!=='number' ){
+        return 'ERROR'
+    }
+    let min = Math.min(n1,n2);
+    let max = Math.max(n1,n2);
+    let sum = 0;
+    if(min < 0) return 'ERROR';
+    for(let i = min;i <= max;i++){
+        sum+=i;
+    }
+    return sum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
