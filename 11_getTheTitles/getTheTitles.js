89d410d7f664208e50a44c16ed5d574096c57cb2
gabr1el1 <gabrielhgxd@gmail.com>
2023-02-28 17:35:58 -0600

    Exercise done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+    titles = books.map(function (book) {
+        return book.title;
+    })
+    return titles;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,31 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    oldest = people.sort(function(a,b){
+        Array.from(arguments).forEach(function(argument){
+            if(argument.yearOfDeath==undefined){
+                argument.yearOfDeath = new Date().getFullYear();
+            }
+        });
+        livedA = a.yearOfDeath - a.yearOfBirth;
+        livedB = b.yearOfDeath - b.yearOfBirth;
+        return livedB- livedA;
+    })[0];
+    return oldest;
 };
-
+findTheOldest([
+    {
+      name: "Carly",
+      yearOfBirth: 2018,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]);
 // Do not edit below this line
 module.exports = findTheOldest;
