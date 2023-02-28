1cd97131090bbe98b60ccf8bcd0eeacd483d1957
gabr1el1 <gabrielhgxd@gmail.com>
2023-02-28 11:07:18 -0600

    Exercise done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+let palindrome;
+let reversed;
+let normalized = "";
+Array.from(str).forEach(function(character){
+    if(character.toUpperCase()!==character.toLowerCase()){
+        normalized+=character;
+    }
+});
+normalized = normalized.toLowerCase()
+reversed = normalized.split("").reverse().join("");
+if(reversed==normalized){
+    return true;
+}else{
+    return false;
+}
 };
-
+palindromes('Racecar!');
 // Do not edit below this line
 module.exports = palindromes;
