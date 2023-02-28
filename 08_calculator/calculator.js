b07922b1cb911f25e8693f5d52c2210c94bd48b1
gabr1el1 <gabrielhgxd@gmail.com>
2023-02-28 08:14:27 -0600

    Exercise done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+  return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1-num2
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  array.forEach(function(elem){
+    sum+=elem;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let mult = 1;
+  array.forEach(function(elem){
+    mult*=elem;
+  });
+  return mult;
 };
 
-const power = function() {
-	
+const power = function(num1,num2) {
+	return num1**num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num==0){
+    return 1;
+  }else{
+    return num * factorial(num-1);
+  }
 };
 
 // Do not edit below this line
