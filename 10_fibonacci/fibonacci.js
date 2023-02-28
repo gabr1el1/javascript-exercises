a6043595062378888cdfca0be2d175f7e8f589b2
gabr1el1 <gabrielhgxd@gmail.com>
2023-02-28 16:28:36 -0600

    Exercise done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,27 @@
-const fibonacci = function() {
-
+const fibonacci = function(nth) {
+    if(nth<1){
+        return "OOPS";
+    }
+    if(nth==1){
+        return 1;
+    }else if(nth==2){
+        return 1
+    }else{
+        //console.log(`fibonacci(nth-1)${nth-1} fibonacci(nth-1)${nth-2}`)
+        return fibonacci(nth-1) + fibonacci(nth-2);
+    }
 };
+fibonacci(3);
+/*
+1
+2
+3
+5
+8
+12
+
+
+*/
 
 // Do not edit below this line
 module.exports = fibonacci;
